ab1e30737ae5f34f718a6d93a45968902f2ed316
shpp-vsmaga <shpp-vsmaga@programming.kr.ua>
2015-07-24 15:29:38 +0200

    ok

---
+++ assignment5-strings/src/com/shpp/cs/vsmaga/Assignment5Task1.java
@@ -0,0 +1,61 @@
+package com.shpp.cs.vsmaga;
+
+/* TODO: This program display number of syllables in word
+* */
+
+import com.shpp.cs.a.console.TextProgram;
+
+public class Assignment5Task1 extends TextProgram{
+    public void run(){
+        /* Repeatedly prompt the user for a word and print out the estimated
+         * number of syllables in that word.
+         */
+        while (true) {
+            String word = readLine("Enter a single word: ");
+            word = filterString(word);
+
+            println("  Syllable count: " + syllablesIn(word));
+        }
+    }
+
+    /*Filter received string - return only letters of the string in lowercase*/
+    private String filterString(String word) {
+        String res = "";
+        for (int i = 0; i < word.length(); i++){
+            if (Character.isLetter(word.charAt(i))){
+                res += Character.toLowerCase(word.charAt(i));
+            }
+        }
+        return res;
+
+    }
+
+    /* Returns number of syllables in the received word*/
+    private int syllablesIn(String word) {
+        int counter = 0; //counter for syllables
+        String vowel = "aeiouy"; //vowel letters of the English alphabet
+
+        for (int i = 0; i < word.length(); i++){
+            if (vowel.contains(Character.toString(word.charAt(i)))) { //if current letter is vowel
+
+                /*If current letters is before penultimate letter and next is not vowel*/
+                if (i < word.length() - 1 && !vowel.contains(Character.toString(word.charAt(i+1)))){
+                    counter++;
+                    
+                /*If current letters is penultimate and next is vowel*/
+                } else if(i == word.length() - 2 && vowel.contains(Character.toString(word.charAt(i+1)))){
+                    counter++;
+
+                /*If current letters is final and not 'E'*/
+                } else if (i == word.length() - 1 && word.charAt(i) != 'e'){
+                    counter++;
+                }
+            }
+        }
+
+        if (counter == 0){
+            return 1;
+        }
+        return counter;
+    }
+}
